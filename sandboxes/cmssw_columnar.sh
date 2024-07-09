a94d1917a7566a3c85f16efd70fe5a2c48803411
Jacopo Malvaso <jacopo.malvaso@stud.unifi.it>
2024-07-09 09:06:29 +0200

    Submitting to lxplus9

--- sandboxes/cmssw_columnar.sh
+++ sandboxes/cmssw_columnar.sh
@@ -13,8 +13,10 @@ action() {
 
     # set variables and source the generic CMSSW setup
     export CF_SANDBOX_FILE="${CF_SANDBOX_FILE:-${this_file}}"
-    export CF_SCRAM_ARCH="$( [ "${os_version}" = "8" ] && echo "el8" || echo "slc7" )_amd64_gcc10"
-    export CF_CMSSW_VERSION="CMSSW_12_6_2"
+    export CF_SCRAM_ARCH=el9_amd64_gcc12
+    export CF_CMSSW_VERSION=CMSSW_14_1_0_pre4    
+    # export CF_SCRAM_ARCH="$( [ "${os_version}" = "8" ] && echo "el8" || echo "slc7" )_amd64_gcc10"
+    # export CF_CMSSW_VERSION="CMSSW_12_6_2"
     export CF_CMSSW_ENV_NAME="$( basename "${this_file%.sh}" )"
     export CF_CMSSW_FLAG="1"  # increment when content changed
 
